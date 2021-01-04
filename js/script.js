51a11f057ffa2f0118bfc3d1c58d81b53f55d597
Atul <support@voorent.com>
2021-01-04 13:06:43 +0530

    updated navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 
+var navigate = {
+  next : '',
+  prev : '',
+  total : 0
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  checkParams();
+});
+
 function checkParams() {
   let urlParams = new URLSearchParams(window.location.search);
   let searchText = urlParams.get("search");
@@ -7,32 +17,6 @@ function checkParams() {
     search(searchText);
   }
 }
-
-function search(searchText) {
-  let item = searchText || document.getElementById("suggestion").value;
-  console.log(item);
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      showSuggestions(JSON.parse(xhttp.responseText));
-      scrollToView();
-    }
-  };
-  xhttp.open("GET", `https://cors-anywhere.herokuapp.com/https://api.lyrics.ovh/suggest/${item}`, true);
-  xhttp.send();
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  checkParams();
-});
-
-var navigate = {
-  next : '',
-  prev : '',
-  total : 0
-}
-
-console.log(navigate);
 
 function networkRequest(url){
   var xhttp = new XMLHttpRequest();
@@ -53,12 +37,17 @@ function showSuggestions(songs) {
   
   if(typeof songs.next != 'undefined'){
     navigate.next =  songs.next;
+  }else{
+    navigate.next = '';
   }
 
   if(typeof songs.prev != 'undefined'){
     navigate.prev =  songs.prev;
+  }else{
+    navigate.prev = '';
   }
 
+  navigate.total = songs.total;
 
   songs.data.forEach((elem) => {
     var template = `<div style="width:48%;float:left;padding:10px;margin-top:10px">
@@ -86,9 +75,9 @@ function showSuggestions(songs) {
   });
 
   inject += `<div style="padding: 20px;clear: both;text-align: center;font-weight: bold;font-size: 22px;">
-      <span onclick="paginate('prev')" class="link">Prev</span>
-      <span onclick="paginate('next')" class="link" style="margin-left:20px">Next</span>
-    </div>`;
+              <span onclick="paginate('prev')" class="link">Prev</span>
+              <span onclick="paginate('next')" class="link" style="margin-left:20px">Next</span>
+            </div>`;
   document.getElementById("songs").innerHTML = inject;
 }
 
@@ -96,7 +85,15 @@ function scrollToView() {
   window.scrollTo(0, document.body.scrollHeight - 1400);
 }
 
-
 function paginate(type) {
-  networkRequest(`https://cors-anywhere.herokuapp.com/${navigate[type]}`);
+  if(navigate[type] == ''){
+    alert("That's it. No more results available.")
+  }else{
+    networkRequest(`https://cors-anywhere.herokuapp.com/${navigate[type]}`);
+  }
+}
+
+function search(searchText) {
+  let item = searchText || document.getElementById("suggestion").value;
+  networkRequest(`https://cors-anywhere.herokuapp.com/https://api.lyrics.ovh/suggest/${item}`);
 }
